d02f52b2702de44e6474a06edb422bd08d70d461
perigrinobruce <chasebruce1992@gmail.com>
2023-01-03 00:37:35 +0000

    rAdded pagination to Get Pokemons and Get Categories

--- Amboosh_Pokemon_Review_Service/Controllers/PokemonController.cs
+++ Amboosh_Pokemon_Review_Service/Controllers/PokemonController.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Amboosh_Pokemon_Review_Service.Data;
 using Amboosh_Pokemon_Review_Service.Dto;
 using Amboosh_Pokemon_Review_Service.Interfaces;
 using Amboosh_Pokemon_Review_Service.Model;
