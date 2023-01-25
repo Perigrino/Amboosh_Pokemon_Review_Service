5bb64f77daedf3ffb855ea1672f0ded29c5595cb
perigrinobruce <chasebruce1992@gmail.com>
2023-01-25 23:11:14 +0000

    Fixing Issue with Db

---
+++ Amboosh_Pokemon_Review_Service/Migrations/20230125230852_intial_Create.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Amboosh_Pokemon_Review_Service.Migrations
+{
+    public partial class intial_Create : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
